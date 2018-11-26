e6ac5ee8c9d013e2d00def77ac107d186d68b96d
Raphaela Mettig <raphasmrocha@users.noreply.github.com>
2018-11-25 19:47:52 -0600

    added plugin

---
+++ linux_atom_scanner.py
@@ -0,0 +1,12 @@
+import volatility.obj as obj
+import volatility.utils as utils
+
+import volatility.plugins.linux.pslist as linux_pslist
+import volatility.plugins.linux.linux_yarascan as linux_yarascan
+
+from volatility.renderers import TreeGrid
+from volatility.renderers.basic import Address
+
+class linux_atom_scanner.py
+
+
